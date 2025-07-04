a530aa18e7d6f8fddb205b221bd3416775c1ac0d
pavel-ryzhov <pavelryzhov06@gmail.com>
2025-07-04 03:25:20 +0300

    sorted

--- main.cpp
+++ main.cpp
@@ -4,19 +4,34 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
-#include <iostream>
 #include <fstream>
 #include <set>
-#include <vector>
+#include <map>
 
-class GlobalVarPrinter : public clang::ast_matchers::MatchFinder::MatchCallback {
+struct CaseInsensitiveCompare {
+    bool operator()(const std::string& s1, const std::string& s2) const {
+        return std::ranges::lexicographical_compare(s1, s2, [](char c1, char c2) {
+            return std::tolower(static_cast<unsigned char>(c1)) <
+                  std::tolower(static_cast<unsigned char>(c2));
+            }
+        );
+    }
+};
+
+class GlobalVarPrinter final : public clang::ast_matchers::MatchFinder::MatchCallback {
 public:
-    std::set<std::string> global_vars;
-    void run(const clang::ast_matchers::MatchFinder::MatchResult &Result) override {
-        const auto* var_decl = Result.Nodes.getNodeAs<clang::VarDecl>("globalVar");
-        if (var_decl && var_decl->isFileVarDecl()) {
-            global_vars.insert(var_decl->getNameAsString());
-        }
+    std::map<std::string, std::set<std::string, CaseInsensitiveCompare>> global_vars;
+
+    void run(const clang::ast_matchers::MatchFinder::MatchResult &result) override {
+        const auto *var_decl = result.Nodes.getNodeAs<clang::VarDecl>("globalVar");
+        if (!var_decl || !var_decl->isFileVarDecl())
+            return;
+        const auto &source_manager = *result.SourceManager;
+        const clang::SourceLocation loc = var_decl->getLocation();
+        if (loc.isInvalid() || !loc.isFileID())
+            return;
+        global_vars[llvm::sys::path::filename(source_manager.getFilename(loc)).str()].insert(
+            var_decl->getNameAsString());
     }
 };
 
@@ -29,39 +44,40 @@ int main(int argc, const char **argv) {
         return 1;
     }
 
-    auto& options_parser = expected_parser.get();
-    auto& compilations = options_parser.getCompilations();
-    const auto& source_paths = options_parser.getSourcePathList();
-    std::ofstream ofs{"output.txt"};
+    auto &options_parser = expected_parser.get();
+    auto &compilations = options_parser.getCompilations();
+    const auto &source_paths = options_parser.getSourcePathList();
+    std::ofstream ofs{"output_sorted.txt"};
 
-    for (const auto& source_path : source_paths) {
-        clang::tooling::ClangTool tool(compilations, {source_path});
+    clang::tooling::ClangTool tool(compilations, source_paths);
 
-        GlobalVarPrinter printer;
+    GlobalVarPrinter printer;
 
-        clang::ast_matchers::DeclarationMatcher global_var_matcher = clang::ast_matchers::varDecl(
-            clang::ast_matchers::isExpansionInMainFile(),
-            clang::ast_matchers::hasGlobalStorage(),
-            clang::ast_matchers::unless(clang::ast_matchers::parmVarDecl()),
-            clang::ast_matchers::unless(clang::ast_matchers::isStaticLocal()),
-            clang::ast_matchers::unless(clang::ast_matchers::isConstexpr()),
-            clang::ast_matchers::unless(clang::ast_matchers::hasType(clang::ast_matchers::isConstQualified()))
-        ).bind("globalVar");
+    clang::ast_matchers::DeclarationMatcher global_var_matcher = clang::ast_matchers::varDecl(
+        clang::ast_matchers::isExpansionInMainFile(),
+        clang::ast_matchers::hasGlobalStorage(),
+        clang::ast_matchers::unless(clang::ast_matchers::parmVarDecl()),
+        clang::ast_matchers::unless(clang::ast_matchers::isStaticLocal()),
+        clang::ast_matchers::unless(clang::ast_matchers::isConstexpr()),
+        clang::ast_matchers::unless(clang::ast_matchers::hasType(clang::ast_matchers::isConstQualified()))
+    ).bind("globalVar");
 
-        clang::ast_matchers::MatchFinder finder;
-        finder.addMatcher(global_var_matcher, &printer);
+    clang::ast_matchers::MatchFinder finder;
+    finder.addMatcher(global_var_matcher, &printer);
 
-        tool.run(clang::tooling::newFrontendActionFactory(&finder).get());
+    tool.run(clang::tooling::newFrontendActionFactory(&finder).get());
 
-        // std::cout << llvm::sys::path::filename(source_path).str() << " ";
-        // for (const auto& name : printer.global_vars)
-        //     std::cout << name << " ";
-        // std::cout << std::endl;
-        ofs << llvm::sys::path::filename(source_path).str() << " ";
-        for (const auto& name : printer.global_vars)
-            ofs << name << " ";
+    for (const auto &file: source_paths) {
+        std::string filename = llvm::sys::path::filename(file).str();
+        ofs << filename << " ";
+        if (const auto it = printer.global_vars.find(filename); it != printer.global_vars.end()) {
+            for (const auto &var_name: it->second) {
+                ofs << var_name << " ";
+            }
+        }
         ofs << std::endl;
     }
+
     ofs.close();
     return 0;
 }
